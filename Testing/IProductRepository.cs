103a0ab7e36e3dc3e02bc371eb3d01b09adb9409
Shrividya Gopi <shrividya.gopi@gmail.com>
2025-05-03 18:59:41 -0400

    Completed

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Testing.Models;
+
+namespace Testing;
+
+public interface IProductRepository
+{
+    public IEnumerable<Product> GetAllProducts();
+    
+}
---
+++ Testing/Models/Product.cs
@@ -0,0 +1,11 @@
+namespace Testing.Models;
+
+public class Product
+{
+    public int ProductId { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public int categoryID { get; set; }
+    public int OnSale { get; set; }
+    public int StockLevel { get; set; }
+}
